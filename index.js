7520688edd66437f7a85e0383680b1171e8fcd74
unknown <sartizayo.on@gmail.com>
2024-06-07 09:00:16 +0600

    base

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,10 +10,47 @@ const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors({
-    origin: ['http://localhost:5173','https://node-93afa.web.app','https://node-93afa.firebaseapp.com'],
+    origin: ['http://localhost:5173'],
     credentials: true
 }
 ));
-app.use(cookieParser());
+
 app.use(express.json());
-app.use(bodyParser.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b6ckjyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+
+const cokkieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production"? true: false,
+    sameSite: process.env.NODE_ENV === "production"? "none":"strict"
+}
+async function run() {
+    try {
+  
+    const UserCollection = client.db("hive").collection("users");
+
+    
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Bees!')
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
